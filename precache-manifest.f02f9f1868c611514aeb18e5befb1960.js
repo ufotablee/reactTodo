a1bdf02fee02669370006b8f21be56f5e317106d
mksninja <kitavaurod@gmail.com>
2019-12-06 22:19:30 +0200

    Updates

---
+++ precache-manifest.f02f9f1868c611514aeb18e5befb1960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29260f108da4aa15d9b6ec39319c3fa2",
+    "url": "/reactTodo/index.html"
+  },
+  {
+    "revision": "572b8c7f490b4e962e22",
+    "url": "/reactTodo/static/css/main.36e7e85e.chunk.css"
+  },
+  {
+    "revision": "fffa029509c878496adf",
+    "url": "/reactTodo/static/js/2.9f8f1bce.chunk.js"
+  },
+  {
+    "revision": "d85a7f66501d00f770ee",
+    "url": "/reactTodo/static/js/3.73a5466b.chunk.js"
+  },
+  {
+    "revision": "572b8c7f490b4e962e22",
+    "url": "/reactTodo/static/js/main.fe377db4.chunk.js"
+  },
+  {
+    "revision": "9602e43af1167c43ee45",
+    "url": "/reactTodo/static/js/runtime-main.941b1b52.js"
+  }
+]);
